df5d5a3da3dc7d1f4b92860b84c183e31682a3ad
Laurence J. Golding <larrygolding@comcast.net>
2016-02-15 05:14:08 -0800

    Use Theory for reader test cases

--- src/ObjectModel.Tests/SchemaReaderTests.cs
+++ src/ObjectModel.Tests/SchemaReaderTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Mount Baker Software.  All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.IO;
 using FluentAssertions;
 using Xunit;
@@ -9,22 +10,32 @@ namespace MountBaker.JSchema.ObjectModel.Tests
 {
     public class SchemaReaderTests
     {
-        [Fact]
-        public void EmptySchema()
+        public static IEnumerable<object[]> TestCases = new[]
         {
-            string jsonText = File.ReadAllText(@"TestData\Empty.schema.json");
-            JsonSchema schema = SchemaReader.ReadSchema(jsonText);
+            new object[]
+            {
+                "Empty",
+                new JsonSchema()
+            },
 
-            schema.Should().Be(new JsonSchema());
-        }
+            new object[]
+            {
+                "BasicProperties",
+                new JsonSchema
+                {
+                    SchemaVersion = JsonSchema.V4Draft
+                }
+            }
+        };
 
-        [Fact]
-        public void SchemaWithOnlyV4Property()
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void CanReadSchema(string fileNameStem, JsonSchema expected)
         {
-            string jsonText = File.ReadAllText(@"TestData\V4.schema.json");
-            JsonSchema schema = SchemaReader.ReadSchema(jsonText);
+            string jsonText = File.ReadAllText($"TestData\\{fileNameStem}.schema.json");
+            JsonSchema actual = SchemaReader.ReadSchema(jsonText);
 
-            schema.Should().Be(new JsonSchema { SchemaVersion = JsonSchema.V4Draft });
+            actual.Should().Be(expected);
         }
     }
 }
