033af97f8f9f9816df5f5d9d5c15ff2dcca43ea0
Larry Golding <lgolding@microsoft.com>
2016-03-20 10:45:44 -0700

    BuildAndTest.cmd builds Release

--- BuildAndTest.cmd
+++ BuildAndTest.cmd
@@ -3,19 +3,21 @@ setlocal
 
 rmdir /s /q bld
 
-msbuild /verbosity:minimal /target:rebuild src\Everything.sln
+set Configuration=Release
+
+msbuild /verbosity:minimal /target:rebuild src\Everything.sln /p:Configuration=%Configuration%
 if "%ERRORLEVEL%" NEQ "0" (
 goto ExitFailed
 )
 
 set XUNIT=src\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe
 
-%XUNIT% bld\bin\Json.Schema.ToDotNet.UnitTests\AnyCPU_Debug\Microsoft.Json.Schema.ToDotNet.UnitTests.dll
+%XUNIT% bld\bin\Json.Schema.ToDotNet.UnitTests\AnyCPU_%Configuration%\Microsoft.Json.Schema.ToDotNet.UnitTests.dll
 if "%ERRORLEVEL%" NEQ "0" (
 goto ExitFailed
 )
 
-%XUNIT% bld\bin\Json.Schema.UnitTests\AnyCPU_Debug\Microsoft.Json.Schema.UnitTests.dll
+%XUNIT% bld\bin\Json.Schema.UnitTests\AnyCPU_%Configuration%\Microsoft.Json.Schema.UnitTests.dll
 if "%ERRORLEVEL%" NEQ "0" (
 goto ExitFailed
 )
