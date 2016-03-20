71935e845497fe92750b5694cc4b1ace36ee91a6
Larry Golding <lgolding@microsoft.com>
2016-03-20 06:05:39 -0700

    Add project Json.Schema.ToDotNet

---
+++ src/Json.Schema.ToDotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("JSON Schema To .NET Library")]
+[assembly: AssemblyDescription("Classes for generating .NET classes from a JSON Schema")]
+
+[assembly: InternalsVisibleTo("Microsoft.JSchema.Tests")]
+
+// This allows Moq to mock internal interfaces of assemblies that are not strong named.
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
