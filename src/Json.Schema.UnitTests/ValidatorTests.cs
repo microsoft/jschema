0ca0b763c25f2a223f089c325d5204af5f4f8c25
Larry Golding <lgolding@microsoft.com>
2016-04-09 06:53:59 -0700

    Add validator class; validate "required"

---
+++ src/Json.Schema.UnitTests/ValidatorTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.Json.Schema.UnitTests
+{
+    public class ValidatorTests
+    {
+        public static object[] TestCases = new object[]
+        {
+            new object[]
+            {
+@"{
+}",
+
+@"{
+}",
+                new string[0]
+            },
+
+            new object[]
+            {
+@"{
+  ""properties"": {
+    ""a"": {
+        ""type"": ""integer""
+    },
+    ""b"": {
+        ""type"": ""integer""
+    },
+    ""c"": {
+        ""type"": ""integer""
+    }
+  },
+  ""required"": [""a"", ""c""]
+}",
+
+@"{
+  ""a"": 1,
+  ""c"": 2
+}",
+
+                new string[0]
+            },
+
+            new object[]
+            {
+@"{
+  ""properties"": {
+    ""a"": {
+        ""type"": ""integer""
+    },
+    ""b"": {
+        ""type"": ""integer""
+    },
+    ""c"": {
+        ""type"": ""integer""
+    }
+  },
+  ""required"": [""a"", ""c""]
+}",
+
+@"{
+  ""b"": 2
+}",
+
+                new string[]
+                {
+                    "The object at path \"\" does not contain the required property \"a\".",
+                    "The object at path \"\" does not contain the required property \"c\"."
+                }
+            }
+        };
+
+        [Theory(DisplayName = "Validator tests")]
+        [MemberData(nameof(TestCases))]
+        public void ReportsMissingRequiredProperty(string schemaText, string instanceText, string[] expectedMessages)
+        {
+            JsonSchema schema = SchemaReader.ReadSchema(schemaText);
+            var target = new Validator(schema);
+            string[] actualMessages = target.Validate(instanceText);
+
+            actualMessages.Length.Should().Be(expectedMessages.Length);
+            actualMessages.Should().ContainInOrder(expectedMessages);
+        }
+    }
+}
---
+++ src/Json.Schema/Validator.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Json.Schema
+{
+    /// <summary>
+    /// Validates JSON documents against a JSON schema.
+    /// </summary>
+    public class Validator
+    {
+        private readonly JsonSchema _schema;
+        private List<string> _messages;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Validator"/> class against
+        /// </summary>
+        /// <param name="schema"></param>
+        public Validator(JsonSchema schema)
+        {
+            if (schema == null)
+            {
+                throw new ArgumentNullException(nameof(schema));
+            }
+
+            _schema = schema;
+        }
+
+        public string[] Validate(string instanceText)
+        {
+            _messages = new List<string>();
+
+            using (var reader = new StringReader(instanceText))
+            {
+                JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+                Validate(o, _schema);
+            }
+
+            return _messages.ToArray();
+        }
+
+        private void Validate(JObject o, JsonSchema _schema)
+        {
+            ValidateRequiredProperties(o, _schema);
+        }
+
+        private void ValidateRequiredProperties(JObject o, JsonSchema _schema)
+        {
+            if (_schema.Required == null)
+            {
+                return;
+            }
+
+            string[] propertyNames = o.Properties().Select(p => p.Name).ToArray();
+
+            foreach (string requiredPropertyName in _schema.Required)
+            {
+                if (!propertyNames.Contains(requiredPropertyName))
+                {
+                    _messages.Add($"The object at path \"{o.Path}\" does not contain the required property \"{requiredPropertyName}\".");
+                }
+            }
+        }
+    }
+}
