49b739ce5977e1f24d3c55241c8d194bd0bbee24
Larry Golding <lgolding@microsoft.com>
2016-05-18 16:47:59 -0700

    Start on validation suite test
    Doesn't work yet since my schema OM can't yet even deserialize
    all valid schemas.

--- src/Json.Schema.ValidationSuiteTests/ValidationSuite.cs
+++ src/Json.Schema.ValidationSuiteTests/ValidationSuite.cs
@@ -1,12 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-namespace Json.Schema.ValidationSuiteTests
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit;
+using JsonSchema = Microsoft.Json.Schema.JsonSchema;
+
+namespace Microsoft.Json.Schema.ValidationSuiteTests
 {
-    public class ValidationSuite
+    public class ValidationSuiteTests
     {
+        [Theory(DisplayName = nameof(ValidationSuite))]
+        [ClassData(typeof(ValidationData))]
+        public void ValidationSuite(TestSuite testSuite)
+        {
+            testSuite.Description.Length.Should().BeGreaterThan(10);
+        }
+    }
+
+    public class ValidationData : IEnumerable<object[]>
+    {
+        private const string TestSuitePath = @"G:\Code\JSON-Schema-Test-Suite\tests\draft4";
+
+        private readonly List<object[]> _data;
+
+        public ValidationData()
+        {
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                ContractResolver = new JsonSchemaContractResolver()
+            };
+
+            _data = new List<object[]>();
+
+            string[] testFiles = Directory.GetFiles(TestSuitePath, "*.json");
+            foreach (string testFile in testFiles)
+            {
+                List<TestSuite> testSuites = JsonConvert.DeserializeObject<List<TestSuite>>(File.ReadAllText(testFile));
+                foreach (TestSuite testSuite in testSuites)
+                {
+                    _data.Add(new object[] { testSuite });
+                }
+            }
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class TestSuite
+    {
+        public string Description { get; set; }
+        public JsonSchema Schema { get; set; } 
+        public List<TestCase> TestCases { get; set; }
+    }
+
+    public class TestCase
+    {
+        public string Description { get; set; }
+        public string Data { get; set; }
+        public bool Valid { get; set; }
     }
 }
