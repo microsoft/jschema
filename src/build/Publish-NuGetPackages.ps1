a38c1b1d6907db945f28cdd6af8a336a8fef7871
Larry Golding <lgolding@microsoft.com>
2016-05-24 14:15:27 -0700

    0.37.0: Json.Schema targets 4.5.1 instead of 4.5.2
    Publish Json.Schema only; no change to Json.Schema.ToDotNet

--- src/build/Publish-NuGetPackages.ps1
+++ src/build/Publish-NuGetPackages.ps1
@@ -11,6 +11,37 @@ param(
     [Parameter(Mandatory=$false)] $PackageSource = "https://nuget.org"
 )
 
+function Prompt-Publish($packageName) {
+    $title = "Publish Package $packageName"
+    $message = "OK to publish ${packageName}?"
+
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Publish"
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Do not publish"
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]] ($yes, $no)
+
+    $Host.UI.PromptForChoice($title, $message, $options, 0)
+}
+
+function Publish-Package($packageName) {
+    $result = Prompt-Publish $packageName
+    if ($result -eq 0) {
+        $nupkg = "$packageName.$packageVersion.nupkg"
+        & $nugetExe push $packagesDirectory\$nupkg $ApiKey -Source $PackageSource
+        if (!$?) {
+            Write-Host -ForegroundColor Red "Error: failed to push $nupkg to $PackageSource"
+            exit 1;
+        }
+        & $nugetExe delete $packageName $packageVersion $ApiKey -Source $PackageSource
+        if (!$?) {
+            Write-Host -ForegroundColor Red "Error: failed to delist $nupkg from $PackageSource"
+            exit 1;
+        }
+    } else {
+        Write-Host -ForegroundColor Yellow "Package $packageName was not published."
+    }
+}
+
 $packagesDirectory = "$PSScriptRoot\..\..\bld\bin\NuGet\Release"
 if (-not (Test-Path $packagesDirectory)) {
     Write-Host -ForegroundColor Red "Error: The NuGet output directory '$packagesDirectory' does not exist. Have you built?"
@@ -28,16 +59,4 @@ $packageVersion = "$major.$minor.$patch$preRelease"
 
 $packages = "Microsoft.Json.Schema", "Microsoft.Json.Schema.ToDotNet"
 
-$packages | ForEach-Object {
-    $nupkg = "$_.$packageVersion.nupkg"
-    & $nugetExe push $packagesDirectory\$nupkg $ApiKey -Source $PackageSource
-    if (!$?) {
-        Write-Host -ForegroundColor Red "Error: failed to push $nupkg to $PackageSource"
-        exit 1;
-    }
-    & $nugetExe delete $_ $packageVersion $ApiKey -Source $PackageSource
-    if (!$?) {
-        Write-Host -ForegroundColor Red "Error: failed to delist $nupkg from $PackageSource"
-        exit 1;
-    }
-}
+$packages | ForEach-Object { Publish-Package $_ }
