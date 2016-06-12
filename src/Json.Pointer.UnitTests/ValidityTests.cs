6b90b16d19140ecff5e7b86b1507013385f2f1f1
Larry Golding <lgolding@microsoft.com>
2016-06-11 19:00:57 -0700

    Add FluentAssertions, Xunit, and trivial test for JsonPointer.

---
+++ src/Json.Pointer.UnitTests/ValidityTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using FluentAssertions;
+using Xunit;
+
+namespace Json.Pointer.UnitTests
+{
+    public class ValidityTests
+    {
+        [Fact(DisplayName = "JsonPointer validity")]
+        public void RunValidityTests()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
