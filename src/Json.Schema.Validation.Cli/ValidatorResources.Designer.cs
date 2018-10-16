7d573b2b7c28a21dac6f5e15480e24bb486892c1
rtaket <rtaket@microsoft.com>
2018-10-16 08:39:40 -0700

    embed resx resources (#53)

--- src/Json.Schema.Validation.Cli/ValidatorResources.Designer.cs
+++ src/Json.Schema.Validation.Cli/ValidatorResources.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Json.Schema.Validation.CommandLine {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Json.Schema.JsonSchemaValidator.ValidatorResources", typeof(ValidatorResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Json.Schema.Validation.CommandLine.ValidatorResources", typeof(ValidatorResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
