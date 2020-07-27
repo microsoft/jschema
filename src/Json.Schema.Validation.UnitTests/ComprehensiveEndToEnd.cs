65c55b6c36ea8d6027179f437d9a413cda1ea5b7
Larry Golding <lgolding@comcast.net>
2020-07-27 16:14:58 -0700

    Compare actual validation results to expected.

--- src/Json.Schema.Validation.UnitTests/ComprehensiveEndToEnd.cs
+++ src/Json.Schema.Validation.UnitTests/ComprehensiveEndToEnd.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+
 using FluentAssertions;
 
+using Microsoft.CodeAnalysis.Sarif;
 using Microsoft.Json.Schema.Validation.CommandLine;
 
 using Xunit;
@@ -22,6 +25,26 @@ namespace Microsoft.Json.Schema.Validation.UnitTests
             });
 
             exitCode.Should().Be((int)Program.ExitCode.Invalid);
+
+            SarifLog actualLog = SarifLog.Load(@"TestData\ComprehensiveInstanceDocument-validation-actual.sarif");
+            SarifLog expectedLog = SarifLog.Load(@"TestData\ComprehensiveInstanceDocument-validation-expected.sarif");
+
+            // Just compare the results, because there's lots of non-deterministic stuff in the
+            // invocation, driver, and artifacts.
+            IList<Result> actualResults = actualLog.Runs[0].Results;
+            IList<Result> expectedResults = expectedLog.Runs[0].Results;
+
+            actualResults.Count.Should().Be(expectedResults.Count);
+            for (int i = 0; i < actualResults.Count; i++)
+            {
+                // The validator sets the result's location URI to an absolute URI, which
+                // varies across machines. So don't try to compare them.
+                actualResults[i].Locations[0].PhysicalLocation.ArtifactLocation.Uri = null;
+                expectedResults[i].Locations[0].PhysicalLocation.ArtifactLocation.Uri = null;
+
+                actualResults[i].ValueEquals(expectedResults[i]).Should().BeTrue();
+            }
         }
+
     }
 }
