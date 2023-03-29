7eea29f51c00bff6068c3937a15699535d69be7d
Shaopeng Li <v-shaopengli@microsoft.com>
2023-03-28 21:06:52 -0700

    PropertyTypeHintTests

---
+++ src/Json.Schema.ToDotNet.UnitTests/Hints/PropertyTypeHintTests.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.Json.Schema.ToDotNet.Hints.UnitTests
+{
+    public class PropertyTypeHintTests : HintTestBase
+    {
+        public static readonly TheoryData<HintTestCase> TestCases = new TheoryData<HintTestCase>
+        {
+            new HintTestCase(
+                "Specifies property name",
+@"{
+  ""type"": ""object"",
+  ""properties"": {
+    ""TheNullableBigIntegerProperty"": {
+      ""type"": ""integer""
+    },
+    ""TheBigIntegerProperty"": {
+      ""type"": ""integer"",
+      ""default"": ""-1"",
+    },
+    ""TheIntegerProperty"": {
+      ""type"": ""integer"",
+      ""default"": ""-1"",
+    },
+    ""TheLongProperty"": {
+      ""type"": ""integer"",
+      ""default"": ""-1"",
+    },
+    ""TheStringProperty"": {
+      ""type"": ""integer"",
+      ""default"": ""-1"",
+    },
+  }
+}",
+
+@"{
+  ""C.TheNullableBigIntegerProperty"": [
+    {
+      ""kind"": ""PropertyTypeHint"",
+      ""arguments"": {
+        ""typeName"": ""BigInteger""
+      }
+    }
+  ],
+  ""C.TheLongProperty"": [
+    {
+      ""kind"": ""PropertyTypeHint"",
+      ""arguments"": {
+        ""typeName"": ""Long""
+      }
+    }
+  ],
+  ""C.TheStringProperty"": [
+    {
+      ""kind"": ""PropertyTypeHint"",
+      ""arguments"": {
+        ""typeName"": ""String""
+      }
+    }
+  ]
+}",
+
+@"using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace N
+{
+    [DataContract]
+    [GeneratedCode(""Microsoft.Json.Schema.ToDotNet"", """ + VersionConstants.FileVersion + @""")]
+    public partial class C
+    {
+        [DataMember(Name = ""TheNullableBigIntegerProperty"", IsRequired = false, EmitDefaultValue = false)]
+        public BigInteger? TheNullableBigIntegerProperty { get; set; }
+        [DataMember(Name = ""TheBigIntegerProperty"", IsRequired = false, EmitDefaultValue = false)]
+        [DefaultValue(""-1"")]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public int TheBigIntegerProperty { get; set; }
+        [DataMember(Name = ""TheIntegerProperty"", IsRequired = false, EmitDefaultValue = false)]
+        [DefaultValue(""-1"")]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public int TheIntegerProperty { get; set; }
+        [DataMember(Name = ""TheLongProperty"", IsRequired = false, EmitDefaultValue = false)]
+        [DefaultValue(""-1"")]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public long TheLongProperty { get; set; }
+        [DataMember(Name = ""TheStringProperty"", IsRequired = false, EmitDefaultValue = false)]
+        [DefaultValue(""-1"")]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public int TheStringProperty { get; set; }
+    }
+}"
+            )
+        };
+
+        [Theory(DisplayName = nameof(PropertyNameHint))]
+        [MemberData(nameof(TestCases))]
+        public void PropertyNameHint(HintTestCase testCase)
+        {
+            RunHintTestCase(testCase);
+        }
+    }
+}
